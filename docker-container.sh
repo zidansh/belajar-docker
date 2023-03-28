93a558e6a8e1a6ee9810058d46b1efb228e499b4
zidansh <zidansh19@gmail.com>
2023-03-28 12:18:52 +0700

    add docker-container.sh

---
+++ docker-container.sh
@@ -0,0 +1,11 @@
+docker container ls -a
+
+docker container ls
+
+docker container create --name contohredis redis:latest
+
+docker container start contohredis
+
+docker container stop contohredis
+
+docker container rm docker-tutorial
